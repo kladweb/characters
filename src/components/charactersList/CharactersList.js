34a510898f78174ce62efc1a88d5c5834397f22c
Pavel Kladkevich <kladpav@gmail.com>
2023-07-28 08:21:57 +0300

    add dynamic loading contend

--- src/components/charactersList/CharactersList.js
+++ src/components/charactersList/CharactersList.js
@@ -5,23 +5,33 @@ import Character from '../character/Character';
 import { useHttp } from '../../hooks/http.hook';
 
 function CharactersList() {
+
   const {loading, request, error, clearError} = useHttp();
-  const _apiBase = 'https://rickandmortyapi.com/api/character/?page=1';
+  const _apiBase = 'https://rickandmortyapi.com/api/character/?page=';
 
   const [charItems, changeCharItems] = useState([]);
-
-  const getCharacters = async () => {
-    const data = await request(_apiBase, 'GET');
-    return data.results;
-  }
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(1);
+  const [fetching, setFetching] = useState(true);
 
   useEffect(() => {
-    getCharacters()
-      .then((data) => {
-        changeCharItems(data);
-        console.log(data);
-      })
+    document.addEventListener('scroll', scrollHandler)
+    return () => {
+      document.removeEventListener('scroll', scrollHandler)
+    }
   }, []);
+
+  useEffect(() => {
+    if (fetching && currentPage <= totalPages) {
+      request(_apiBase + currentPage, 'GET')
+        .then((data) => {
+          setTotalPages(data.info.pages);
+          changeCharItems([...charItems, ...data.results]);
+          setCurrentPage(currentPage + 1);
+        })
+        .finally(() => setFetching(false));
+    }
+  }, [fetching]);
 
   const characters = charItems.map(character =>
     <Character
@@ -29,13 +39,19 @@ function CharactersList() {
       name={character.name}
       image={character.image}
     />
-  )
+  );
+
+  const scrollHandler = (e) => {
+    if (e.target.documentElement.scrollHeight - (e.target.documentElement.scrollTop + window.innerHeight) < 100) {
+      setFetching(true);
+    }
+  }
 
   return (
     <div className='content characters'>
       {characters}
     </div>
-  )
+  );
 }
 
 export default CharactersList;
